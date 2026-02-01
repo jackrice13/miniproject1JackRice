55f07b5186ea8e66219fce4648f92e5dc0acda1e
jackrice13 <jackrice13@gmail.com>
2026-01-31 22:24:53 -0500

    np and plt added, and working in concept

--- main.py
+++ main.py
@@ -3,10 +3,10 @@
 ### Mini Project 1
 # This project will be using the packages NumPy and Matplotlib in order to create 5 graphs that output as PNG files.
 #
-#     (5/5 points) Initial comments with your name, class and project at the top of your .py file.
-#     (5/5 points) Proper import of packages used.
-#     (20/20 points) Using an API of your choice (yfinance works), collect the closing price of 5 of your favorite stock tickers for the last 10 trading days.
-#     (10/10 points) Store this information in a list that you will convert to a array in NumPy.
+#     x(5/5 points) Initial comments with your name, class and project at the top of your .py file.
+#     x(5/5 points) Proper import of packages used.
+#     x(20/20 points) Using an API of your choice (yfinance works), collect the closing price of 5 of your favorite stock tickers for the last 10 trading days.
+#     x(10/10 points) Store this information in a list that you will convert to a array in NumPy.
 #     (10/10 points) Plot these 5 graphs. Feel free to add as much information to the graphs as you like exploring the documentation for matplotlib. At minimum it just needs to show 10 data points.
 #     (10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
 #     (10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
@@ -15,8 +15,9 @@
 
 import yfinance as yf
 import pprint
-#import numpy as np
-# import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+
 # import copy
 mystocks=['MSFT','AAPL','V','GOOG','KO','META']
 mystockdata = { }
@@ -24,6 +25,21 @@ mystockdata = { }
 for stock in mystocks:
     dat = yf.Ticker(stock)
     last10 = dat.history(period="10d")
-    mystockdata[stock] = last10['Close']
+    mystockdata[stock] = []
+    for price in last10['Close']:
+        mystockdata[stock].append(price)
+    # mystockdata[stock] = last10['Close']
+    mystocknp=np.array(mystockdata[stock])
+    print(mystocknp)
 
-pprint.pprint(mystockdata)
+
+
+    plt.plot(mystocknp)
+    plt.ylabel('Closing Price')
+    plt.show()
+
+
+
+
+    # a = np.array([2, 3, 4])
+    # pprint.pprint(mystockdata)
